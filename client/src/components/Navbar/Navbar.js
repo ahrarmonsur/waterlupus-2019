77ce670c758b46a5c2c3bf876e62d32e066c83b4
Ahrar Monsur <ahrarmonsur93@gmail.com>
2019-05-25 10:38:49 -0400

    Adds navbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+// import './Navbar.sass';
+
+class Navbar extends React.Component {
+
+	render() {
+		return (
+			<AppBar>Monarch</AppBar>
+		);
+	}
+}
+
+export default Navbar;
+
