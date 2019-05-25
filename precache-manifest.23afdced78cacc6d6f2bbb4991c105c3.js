10577bb43c44070f9dcf74b738d430b78e2d11d9
Ahrar Monsur <ahrarmonsur93@gmail.com>
2019-05-25 13:26:26 -0400

    Updates

---
+++ precache-manifest.23afdced78cacc6d6f2bbb4991c105c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b323393873efc2db9f20bd4fc865d278",
+    "url": "/waterlupus-2019/index.html"
+  },
+  {
+    "revision": "1549529961485ee616fc",
+    "url": "/waterlupus-2019/static/css/main.61d26e77.chunk.css"
+  },
+  {
+    "revision": "da2ea603c6ef3e175400",
+    "url": "/waterlupus-2019/static/js/2.61d477c5.chunk.js"
+  },
+  {
+    "revision": "1549529961485ee616fc",
+    "url": "/waterlupus-2019/static/js/main.bf4ab0b0.chunk.js"
+  },
+  {
+    "revision": "01c26bb058fa0d8c08f2",
+    "url": "/waterlupus-2019/static/js/runtime~main.fff56dfb.js"
+  },
+  {
+    "revision": "d2baf6abafa17a780e7fda64ec5d03fc",
+    "url": "/waterlupus-2019/static/media/humans_1.d2baf6ab.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/waterlupus-2019/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aa0db789ab38ebf4e78203636e3ead6d",
+    "url": "/waterlupus-2019/static/media/value1.aa0db789.svg"
+  }
+]);
